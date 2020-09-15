b192d68d04a397fe306ef0bdac8fcf54297359e3
akharin <akharin@itms-perm.ru>
2020-09-15 15:24:10 +0500

    @typescript-eslint/unbound-method ignoreStatic

--- packages/eslint-config-ts/.eslintrc.js
+++ packages/eslint-config-ts/.eslintrc.js
@@ -69,6 +69,12 @@ module.exports = {
 		'@typescript-eslint/triple-slash-reference': 'error',
 		'@typescript-eslint/type-annotation-spacing': 'error',
 		'@typescript-eslint/unified-signatures': 'error',
+		'@typescript-eslint/unbound-method': [
+			'error',
+			{
+				'ignoreStatic': true
+			}
+		],
 		'camelcase': 'off'
 	}
 };
